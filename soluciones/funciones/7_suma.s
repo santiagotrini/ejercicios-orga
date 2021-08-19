1918c9ce6065eb00afb03e4b82da34bf7a42bc3c
Santiago Trini <santiagotrini@gmail.com>
2021-08-18 21:29:47 -0300

    termina soluciones de funciones

--- soluciones/funciones/7_suma.s
+++ soluciones/funciones/7_suma.s
@@ -1,7 +1,29 @@
 .data
+prompt: .asciiz "Ingrese un numero\n"
+nl: .asciiz "\n"
 
+.text
 .globl main
 main:
+  li $v0, 4
+  la $a0, prompt
+  syscall
+  li $v0, 5
+  syscall
+  move $a0, $v0
+  jal suma_n
+  move $a0, $v0
+  li $v0, 1
+  syscall
+  li $v0, 4
+  la $a0, nl
+  syscall
+  li $v0, 10
+  syscall
 
 # funcion suma_n(n)
 suma_n:
+  addi $t0, $a0, 1   # (n + 1)
+  mul $v0, $a0, $t0  # n(n + 1)
+  div $v0, $v0, 2    # n(n + 1) / 2
+  jr $ra
