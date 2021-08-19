0d704d5ea6eaa478e549b972947d226715ecf7b0
Santiago Trini <santiagotrini@gmail.com>
2021-08-18 21:18:37 -0300

    agrega soluciones

--- soluciones/funciones/5_factorial_iterativo.s
+++ soluciones/funciones/5_factorial_iterativo.s
@@ -1,7 +1,33 @@
 .data
+prompt: .asciiz "Ingrese un numero\n"
+nl: .asciiz "\n"
 
+.text
 .globl main
 main:
+  li $v0, 4
+  la $a0, prompt
+  syscall
+  li $v0, 5
+  syscall
+  move $a0, $v0
+  jal factorial
+  move $a0, $v0
+  li $v0, 1
+  syscall
+  li $v0, 4
+  la $a0, nl
+  syscall
+  li $v0, 10
+  syscall
 
-# funcion factorial (version iterativa)
+# funcion factorial(n) (version iterativa)
 factorial:
+  li $v0, 1
+loop:
+  beq $a0, $zero, return
+  mul $v0, $v0, $a0
+  addi $a0, $a0, -1
+  j loop
+return:
+  jr $ra
--- soluciones/funciones/6_factorial_recursivo.s
+++ soluciones/funciones/6_factorial_recursivo.s
@@ -1,7 +1,43 @@
 .data
+prompt: .asciiz "Ingrese un numero\n"
+nl: .asciiz "\n"
 
+.text
 .globl main
 main:
+  li $v0, 4
+  la $a0, prompt
+  syscall
+  li $v0, 5
+  syscall
+  move $a0, $v0
+  jal factorial
+  move $a0, $v0
+  li $v0, 1
+  syscall
+  li $v0, 4
+  la $a0, nl
+  syscall
+  li $v0, 10
+  syscall
 
-# funcion factorial (version recursiva)
+# funcion factorial(n) (version recursiva)
 factorial:
+  addi $sp, $sp, -8
+  sw $a0, 0($sp)
+  sw $ra, 4($sp)
+  beq $a0, $zero, base
+  addi $a0, $a0, -1
+  jal factorial
+  lw $a0, 0($sp)
+  lw $ra, 4($sp)
+  addi $sp, $sp, 8
+  mul $v0, $v0, $a0
+  j return
+base:
+  li $v0, 1
+  lw $a0, 0($sp)
+  lw $ra, 4($sp)
+  addi $sp, $sp, 8
+return:
+  jr $ra
